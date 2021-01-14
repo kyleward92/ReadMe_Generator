a34702957d23ac93c50889a39af79e200437fada
unknown <kyleward1992@gmail.com>
2021-01-14 12:06:44 -0500

    create function1

--- index.js
+++ index.js
@@ -59,7 +59,14 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("ReadMe was created");
+    })
 }
+
 
 // function to initialize program
 function init() {
